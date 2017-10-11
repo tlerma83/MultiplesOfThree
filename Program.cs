9ff02cf964210fa63141868e89584807aba80b4e
Tamela Lerma <tlerma83@gmail.com>
2017-10-11 16:36:15 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultiplesOfThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            string GetMultiplesOfThree (int startNumber, int endNumber)
+            {
+                string numbersString = "";
+                for (var i = startNumber; i <= endNumber; i++)
+                {
+                    if (i % 3 == 0)
+                    {
+                        numbersString +=(i.ToString() + ",");
+                    }
+                }
+                return numbersString.Remove(numbersString.LastIndexOf(","));
+            }       
+
+            Console.WriteLine(GetMultiplesOfThree(1, 6));
+        
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# MultiplesOfThree
+
+code challenge
+Given the following method layout, finish the method so that it will return comma-separated list of all numbers between the two given numbers that are multiples of 3. Both the start and end number are inclusive. For Instance GetMultiplesOfThree(1, 6) will return ‘3, 6’  
+example...
+- string GetMultiplesOfThree (int startNumber, int endNumber) {}
